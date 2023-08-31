d9c6836721063fbf76dbaa62462c7e39414d7d30
Raymond Baghumian <swordart2050@gmail.com>
2023-09-01 02:40:48 +0430

    Define Auth Model from UserServiceProvider

--- modules/LMS/User/Providers/UserServiceProvider.php
+++ modules/LMS/User/Providers/UserServiceProvider.php
@@ -2,10 +2,17 @@
 
 namespace modules\LMS\User\Providers;
 
+
 use Illuminate\Support\ServiceProvider;
+use modules\LMS\User\Models\User;
 
 class UserServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        config()->set('auth.providers.users.model', User::class);
+    }
+
     public function boot(): void
     {
         $this->loadRoutesFrom(__DIR__ . '/../Routes/user_route.php');
