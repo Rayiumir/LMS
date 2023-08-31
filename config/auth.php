decac4c6a252bee5da2c273c371ffd129424604c
Raymond Baghumian <swordart2050@gmail.com>
2023-09-01 02:37:17 +0430

    Transfer Models for User modules

--- config/auth.php
+++ config/auth.php
@@ -62,7 +62,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => \modules\LMS\User\Models\User::class,
         ],
 
         // 'users' => [
--- modules/LMS/User/Http/Controllers/Auth/RegisterController.php
+++ modules/LMS/User/Http/Controllers/Auth/RegisterController.php
@@ -3,11 +3,11 @@
 namespace modules\LMS\User\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use modules\LMS\User\Models\User;
 
 class RegisterController extends Controller
 {
@@ -61,7 +61,7 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\Models\User
+     * @return \modules\LMS\User\Models\User
      */
     protected function create(array $data)
     {
