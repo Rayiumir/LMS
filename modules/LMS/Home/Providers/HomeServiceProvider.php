8c4d846eb050c0497310a78aeb0445b2bdc10009
Raymond Baghumian <swordart2050@gmail.com>
2023-09-02 01:59:48 +0430

    Create Module Provider Home

---
+++ modules/LMS/Home/Providers/HomeServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace modules\LMS\Home\Providers;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class HomeServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        //
+    }
+}
