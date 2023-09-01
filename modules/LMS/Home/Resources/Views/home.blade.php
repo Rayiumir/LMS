8adf8db96b295ed516039faf6d99fb4560c68f45
Raymond Baghumian <swordart2050@gmail.com>
2023-09-02 02:23:41 +0430

    Create Module View Components Home

--- modules/LMS/Home/Resources/Views/home.blade.php
+++ modules/LMS/Home/Resources/Views/home.blade.php
@@ -1,4 +1,4 @@
-<x-AppLayout>
+<x-User::AppLayout>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -18,4 +18,4 @@
         </div>
     </div>
 </div>
-</x-AppLayout>
+</x-User::AppLayout>
